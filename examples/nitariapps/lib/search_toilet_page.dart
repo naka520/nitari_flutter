8314bc8d8c3f8f9362d35b9debf97cedf24e35c3
naka520 <yashipipi0520@gmail.com>
2023-08-26 17:30:35 +0900

    init

--- examples/nitariapps/lib/search_toilet_page.dart
+++ examples/nitariapps/lib/search_toilet_page.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class SearchToiletPage extends StatefulWidget {
-  const SearchToiletPage({Key? key}) : super(key: key);
-
-  @override
-  _SearchToiletPageState createState() => _SearchToiletPageState();
-}
-
-class _SearchToiletPageState extends State<SearchToiletPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Image.network('https://lh3.googleusercontent.com/p/AF1QipOYL_tdFgqHRDOTD7jYTPu1coxpigFbKyZlwOji=s1360-w1360-h1020'),
-          Text('トイレ名'),
-          ElevatedButton(
-            onPressed: () {
-              // TODO: GoogleMapへ遷移
-            },
-            child: Text('Google Mapへ'),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class SearchToiletPage extends StatefulWidget {
+//   const SearchToiletPage({Key? key}) : super(key: key);
+//
+//   @override
+//   _SearchToiletPageState createState() => _SearchToiletPageState();
+// }
+//
+// class _SearchToiletPageState extends State<SearchToiletPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Column(
+//         children: [
+//           Image.network('https://lh3.googleusercontent.com/p/AF1QipOYL_tdFgqHRDOTD7jYTPu1coxpigFbKyZlwOji=s1360-w1360-h1020'),
+//           Text('トイレ名'),
+//           ElevatedButton(
+//             onPressed: () {
+//               // TODO: GoogleMapへ遷移
+//             },
+//             child: Text('Google Mapへ'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }　元のソース
